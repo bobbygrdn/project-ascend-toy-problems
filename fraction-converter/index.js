3d307e87f80db432db67edcd46b102b0bf2d69dd
bobbygrdn <bobbygrdn777@gmail.com>
2024-09-09 18:19:24 -0700

    Adds logic to solve problem

--- fraction-converter/index.js
+++ fraction-converter/index.js
@@ -1,5 +1,28 @@
 const fractionConverter = (number) => {
-    return null
+    // Check if the number is a whole number
+    if (Number.isInteger(number)) {
+        return `${number}/1`;
+    };
+
+    // Find decimal places after whole number
+    let decimalPlaces = number.toString().split('.')[1].length;
+
+    // Create a variable for the numerator
+    let numerator = number * (10 ** decimalPlaces);
+    
+    // Create a variable for the denominator
+    let denominator = 1 * (10 ** decimalPlaces);
+
+    // Simplify the fraction
+    for (let i = 2; i <= numerator; i++) {
+        while (numerator % i === 0 && denominator % i === 0) {
+            numerator = numerator / i;
+            denominator = denominator / i;
+        }
+    };
+
+    // Return the simplified fraction
+    return `${numerator}/${denominator}`;
 };
 
 export default fractionConverter;
