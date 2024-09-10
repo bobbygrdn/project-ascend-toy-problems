fbaedac41b9cab7c9a3986633d55d29176342c33
bobbygrdn <bobbygrdn777@gmail.com>
2024-09-09 18:19:49 -0700

    Adds tests to see if function can handle different edge cases

--- fraction-converter/index.test.js
+++ fraction-converter/index.test.js
@@ -1,7 +1,28 @@
 import fractionConverter from ".";
 
 describe("fractionConverter", () => {
-    it('should return null', () => {
-        expect(fractionConverter(0)).toBeNull();
+   it('should return a string form of the fraction', () => {
+        expect(fractionConverter(0.5)).toBe('1/2');
+    });
+    it('should return a string form of the fraction', () => {
+        expect(fractionConverter(0.75)).toBe('3/4');
+    });
+    it('should return a string form of the fraction', () => {
+        expect(fractionConverter(0.999)).toBe('999/1000');
+    });
+    it('should return a string form of the fraction', () => {
+        expect(fractionConverter(0.256)).toBe('32/125');
+    });
+    it('should handle whole numbers', () => {
+        expect(fractionConverter(3.0)).toBe('3/1');
+    });
+    it('should handle irregular fractions', () => {
+        expect(fractionConverter(2.5)).toBe('5/2');
+    });
+    it('should handle irregular fractions', () => {
+        expect(fractionConverter(1.625)).toBe('13/8');
+    });
+    it('should handle large decimal numbers', () => {
+        expect(fractionConverter(9846.948)).toBe('2461737/250');
     });
 });
