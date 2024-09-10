44edf677857e09877072d4518610929f85cb433e
bobbygrdn <bobbygrdn777@gmail.com>
2024-09-09 18:40:08 -0700

    Adds coverage logic for report

--- fraction-converter/jest.config.js
+++ fraction-converter/jest.config.js
@@ -1,5 +1,8 @@
 const config = {
   coverageProvider: "v8",
+  collectCoverage: true,
+  coverageDirectory: "coverage", 
+  coverageReporters: ["html"],
   reporters: ["default", "./utils/custom-reporter.js"],
 };
 
